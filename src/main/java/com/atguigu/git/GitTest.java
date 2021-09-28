43314660027e005b71b48c46b3276c4a14071c91
antonio <Antonio.LiuK@outlook.com>
2021-09-28 18:50:40 +0800

    hot-fix commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("hello git");
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hot-fix test");
     }
 }
