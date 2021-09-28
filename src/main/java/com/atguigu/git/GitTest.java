31171d4c0220dec5dbda9720c7ef793a98cb57e1
antonio <Antonio.LiuK@outlook.com>
2021-09-28 18:41:29 +0800

    second commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,5 +3,6 @@ package com.atguigu.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
